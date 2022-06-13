40a9860714bec3de0d33d7accb9c495211a08987
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-06-13 17:39:45 +0100

    Update serverOwnerOnly.ts

--- src/preconditions/serverOwnerOnly.ts
+++ src/preconditions/serverOwnerOnly.ts
@@ -3,7 +3,14 @@ import type { Interaction, } from 'discord.js';;
 
 export class UserPrecondition extends Precondition {
   public chatInputRun(interaction: Interaction) {
-    return interaction.user.id == interaction.guild?.ownerId ? this.ok() : this.error({ message: 'This can only be used by the guild owner'})
+    if (process.env.NODE_ENV == "dev") {
+      return this.ok()
+    } else if (interaction.user.id == interaction.guild?.ownerId) {
+      return this.ok()
+    } else {
+      return this.error({ message: 'This can only be used by the guild owner'})
+    }
+    //return interaction.user.id == interaction.guild?.ownerId ? this.ok() : this.error({ message: 'This can only be used by the guild owner'})
   }
 }
 
