c8017a8faed0012ba0a30fa79c6853e50d5becdb
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-06-13 19:47:13 +0100

    Update ready + stuff

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,13 +1,40 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Listener, ListenerOptions} from '@sapphire/framework';
-import { blue, bold } from 'colorette';
+import { Listener, ListenerOptions, type Store} from '@sapphire/framework';
+import { createBanner } from '@skyra/start-banner';
 import { botConfig } from '../config.js';
+import figlet from 'figlet';
+import gradient from 'gradient-string';
+import { blue } from 'colorette';
 @ApplyOptions<ListenerOptions>({
   event: "ready",
   once: true
 })
 export class UserEvent extends Listener {
   public run() {
-    console.log(bold(blue(`${botConfig.botName}!`)))
+    console.log(
+      createBanner(
+        {
+          name: [
+            gradient.pastel.multiline(figlet.textSync(botConfig.botName))
+          ],
+          extra: [
+            gradient.pastel(`NODE_ENV=${process.env.NODE_ENV}`),
+            `${this.printStoreDebugInformation()}`
+          ]
+        }
+      )
+    )
+  }
+  private printStoreDebugInformation() {
+    const { client, logger } = this.container;
+    const stores = [...client.stores.values()];
+    const last = stores.pop()!;
+
+    for (const store of stores) logger.info(this.styleStore(store));
+    logger.info(this.styleStore(last));
+  }
+
+  private styleStore(store: Store<any>) {
+   return `${gradient.passion(`Loaded`)} ${process.env.NODE_ENV == "dev" ? blue(`${store.name}`) : gradient.pastel(`${store.name}`)}`
   }
 }
