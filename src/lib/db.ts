7cfe8f8cbde6db1908d0703a307babb55b046f6e
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-03-18 09:15:37 +0000

    Add Mysql DB support

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,29 @@
 import { container } from '@sapphire/framework';
-import { QuickDB } from 'quick.db';
-const db = new QuickDB();
+import { MySQLDriver, QuickDB } from 'quick.db';
+/* const db = new QuickDB();
 container.db = db;
+declare module '@sapphire/pieces' {
+	interface Container {
+		db: any;
+	}
+} */
+if (process.env.NODE_ENV == 'dev') {
+	const db = new QuickDB();
+	container.db = db;
+} else {
+	const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+	// @ts-expect-error
+	const mysql = new MySQLDriver({
+		host: DB_HOST,
+		user: DB_USER,
+		password: DB_PASS,
+		database: DB_NAME
+	})(async () => {
+		await mysql.connect();
+		const db = new QuickDB({ driver: mysql });
+		container.db = db;
+	})();
+}
 declare module '@sapphire/pieces' {
 	interface Container {
 		db: any;
